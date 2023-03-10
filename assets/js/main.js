5f1d830af043471548c5e8396a79fbd1b540a01b
Rizvan555 <rizvan84@gmx.de>
2023-03-10 20:00:47 +0100

    changed some styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,42 +10,44 @@ const berechnen = () => {
   const euroInput = document
     .querySelector(".euroInput")
     .value.replace(",", ".");
-  //   const calculateBtn = document.querySelector("calculateBtn");
   const vatAmountResult = document.querySelector(".VAT-amount-result");
   const totalAmountResult = document.querySelector(".total-amount-result");
   const textNetto = document.querySelector(".text-netto");
   const textBrutto = document.querySelector(".text-brutto");
+  const nettoInfo = document.querySelector(".netto-info");
+  const bruttoInfo = document.querySelector(".brutto-info");
 
   let VATAmount, totalAmount;
 
   if (nettoToBruttoRadio) {
     if (vat19Radio) {
-      VATAmount = (euroInput * 19) / 100;
+      VATAmount = euroInput * 0.19;
       totalAmount = Number(euroInput) + VATAmount;
       VATAmount = VATAmount.toFixed(2);
       totalAmount = totalAmount.toFixed(2);
     } else if (vat7Radio) {
-      VATAmount = (euroInput * 7) / 100;
+      VATAmount = euroInput * 0.07;
       totalAmount = Number(euroInput) + VATAmount;
       VATAmount = VATAmount.toFixed(2);
       totalAmount = totalAmount.toFixed(2);
     }
   } else if (bruttoToNettoRadio) {
     if (vat19Radio) {
-      VATAmount = (euroInput * 19) / 100;
+      VATAmount = euroInput * 0.19;
       totalAmount = Number(euroInput) - VATAmount;
       VATAmount = VATAmount.toFixed(2);
       totalAmount = totalAmount.toFixed(2);
     } else if (vat7Radio) {
-      VATAmount = (euroInput * 7) / 100;
+      VATAmount = euroInput * 0.07;
       totalAmount = Number(euroInput) - VATAmount;
       VATAmount = VATAmount.toFixed(2);
       totalAmount = totalAmount.toFixed(2);
     }
     textNetto.style.display = "block";
     textBrutto.style.display = "none";
+    bruttoInfo.style.display = "block";
+    nettoInfo.style.display = "none";
   }
-
   vatAmountResult.innerHTML = "<span>" + VATAmount + " " + "€" + "</span>";
   totalAmountResult.innerHTML = "<span>" + totalAmount + " " + "€" + "</span>";
 };
